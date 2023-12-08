559e2e9b63f9a3031296b3c0a636ea95ce4974aa
Ankit Patel <Itsakppatel@gmail.com>
2023-12-08 10:51:25 +0530

    Solution of hard problems

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,39 @@
 */
 
 class Todo {
+  constructor() {
+    this.todo = [];
+  }
+  add(todoItem) {
+    this.todo.push(todoItem);
+  }
+  remove(indexOfTodo) {
+    if (this.todo.length === 0) {
+      return this.todo;
+    }
 
+    if (indexOfTodo > -1) {
+      this.todo.splice(indexOfTodo, 1);
+    }
+  }
+  update(index, updatedTodo) {
+    if (index >= this.todo.length - 1) {
+      return this.todo;
+    }
+    this.todo[index] = updatedTodo;
+  }
+  getAll() {
+    return this.todo;
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo >= this.todo.length) {
+      return null;
+    }
+    return this.todo[indexOfTodo];
+  }
+  clear() {
+    this.todo.splice(0, this.todo.length);
+  }
 }
 
 module.exports = Todo;
